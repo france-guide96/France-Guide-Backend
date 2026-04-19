79a8a69b6b30ee9748a8310e3dea6ac1233997d9
ervand96 <ervandharutyunyan06096@gmail.com>
2026-04-20 00:04:14 +0400

    init

---
+++ src/api/tour-card/controllers/tour-card.ts
@@ -0,0 +1,7 @@
+/**
+ * tour-card controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::tour-card.tour-card');
---
+++ src/api/tour-card/routes/tour-card.ts
@@ -0,0 +1,7 @@
+/**
+ * tour-card router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::tour-card.tour-card');
