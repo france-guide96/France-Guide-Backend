f21845fab158bcdf43e5c9ec1e58dd06930fb681
ervand96 <ervandharutyunyan06096@gmail.com>
2026-04-21 12:14:37 +0400

    commit

--- dist/config/server.js
+++ dist/config/server.js
@@ -1,10 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const config = ({ env }) => ({
-    host: env('HOST', '0.0.0.0'),
-    port: env.int('PORT', 1337),
+const config = ({ env, }) => ({
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
     app: {
-        keys: env.array('APP_KEYS'),
+        keys: env.array("APP_KEYS"),
+    },
+    transfer: {
+        remote: { enabled: true },
     },
 });
 exports.default = config;
