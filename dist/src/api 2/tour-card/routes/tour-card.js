248a29b2852408d3523a3894bfacfec61dad0625
ervand96 <ervandharutyunyan06096@gmail.com>
2026-05-05 22:40:43 +0400

    deploy

--- dist/src/api 2/tour-card/routes/tour-card.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * tour-card router
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreRouter('api::tour-card.tour-card');
--- dist/src/api 2/tour-card/services/tour-card.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * tour-card service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::tour-card.tour-card');
