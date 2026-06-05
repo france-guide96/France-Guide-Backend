0aa55478846901d290188dca1914151636f38ca5
ervand96 <ervandharutyunyan06096@gmail.com>
2026-06-05 18:43:03 +0400

    fix: keep responsive dimensions but restrict size limit to 2mb

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,5 +1,14 @@
-import type { Core } from '@strapi/strapi';
+import type { Core } from "@strapi/strapi";
 
-const config = ({ env }: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({});
+const config = ({
+  env,
+}: Core.Config.Shared.ConfigParams): Core.Config.Plugin => ({
+  upload: {
+    config: {
+      responsiveDimensions: true,
+      sizeLimit: 2 * 1024 * 1024,
+    },
+  },
+});
 
 export default config;
